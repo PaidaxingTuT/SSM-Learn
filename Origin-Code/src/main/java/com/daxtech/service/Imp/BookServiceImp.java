61714b1ae3eff6f8592b8b711c725ba335838be0
AmaneKanata <paidaxing_tut@outlook.com>
2026-01-27 15:41:13 +0800

    feat(DI):

--- Origin-Code/src/main/java/com/daxtech/service/Imp/BookServiceImp.java
+++ Origin-Code/src/main/java/com/daxtech/service/Imp/BookServiceImp.java
@@ -1,14 +1,18 @@
 package com.daxtech.service.Imp;
 
 import com.daxtech.mapper.BookMapper;
-import com.daxtech.mapper.Imp.BookMapperImp;
 import com.daxtech.service.BookService;
 
 public class BookServiceImp implements BookService {
-    private BookMapper bookMapper = new BookMapperImp();
+    //5.删除业务层中使用的new对象
+    private BookMapper bookMapper;  // = new BookMapperImp();
     public void save(){
         System.out.println("book service save");
         bookMapper.save();
     }
 
+    //6.提供set方法用于属性注入
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
 }
