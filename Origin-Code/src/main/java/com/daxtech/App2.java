be5f569443d5284bc97b88a3c961b4f399bc8128
AmaneKanata <paidaxing_tut@outlook.com>
2026-01-27 15:52:23 +0800

    feat(Bean的别名):

--- Origin-Code/src/main/java/com/daxtech/App2.java
+++ Origin-Code/src/main/java/com/daxtech/App2.java
@@ -9,7 +9,7 @@ public class App2 {
         //获取IOC容器
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         //获取Bean
-        BookService bookService = (BookService) context.getBean("bookService");
+        BookService bookService = (BookService) context.getBean("service");
 
         bookService.save();
     }
