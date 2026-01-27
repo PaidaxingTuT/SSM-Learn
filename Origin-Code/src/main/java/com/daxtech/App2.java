5934b7d332af7decf33af6947900265755e60dd5
AmaneKanata <paidaxing_tut@outlook.com>
2026-01-27 15:12:01 +0800

    feat(定义IOC):

---
+++ Origin-Code/src/main/java/com/daxtech/App2.java
@@ -0,0 +1,16 @@
+package com.daxtech;
+
+import com.daxtech.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App2 {
+    public static void main(String[] args) {
+        //获取IOC容器
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取Bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        bookService.save();
+    }
+}
