14cb6fe179c12a659fdaf27405189ee61978f6fe
AmaneKanata <paidaxing_tut@outlook.com>
2026-01-30 21:18:59 +0800

    feat(AOP通知类型):

--- SpringAOP/src/main/java/com/daxtech/aop/MyAdvice.java
+++ SpringAOP/src/main/java/com/daxtech/aop/MyAdvice.java
@@ -1,7 +1,8 @@
 package com.daxtech.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +15,22 @@ public class MyAdvice {
 
     }
 
-    @Before("pt()")
+    /*@Before("pt()")
     public void method(){
         System.out.println(System.currentTimeMillis());
+    }*/
+
+    @Around("pt()")
+    public void around(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("begin");
+        pjp.proceed();
+        System.out.println("end");
     }
+
+    // Before 之前
+    // After 之后
+    // Around 前后
+    // AfterReturning 返回之后
+    // AfterThrowing 异常之后
+
 }
