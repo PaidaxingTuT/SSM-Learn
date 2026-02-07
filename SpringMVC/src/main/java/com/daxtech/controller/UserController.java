284527611cdc2bb42e5fb363abbe5d75e8b09b94
AmaneKanata <paidaxing_tut@outlook.com>
2026-02-07 11:18:49 +0800

    feat: RESTful 类注释简化

--- SpringMVC/src/main/java/com/daxtech/controller/UserController.java
+++ SpringMVC/src/main/java/com/daxtech/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.daxtech.controller;
 
 import com.daxtech.pojo.user;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
