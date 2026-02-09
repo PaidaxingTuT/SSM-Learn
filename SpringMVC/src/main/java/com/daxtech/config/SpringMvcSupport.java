6f8c48abe85892e7f22e46862080a544fd5d7360
AmaneKanata <paidaxing_tut@outlook.com>
2026-02-09 14:04:31 +0800

    feat: 配置拦截器类

--- SpringMVC/src/main/java/com/daxtech/config/SpringMvcSupport.java
+++ SpringMVC/src/main/java/com/daxtech/config/SpringMvcSupport.java
@@ -1,15 +1,26 @@
 package com.daxtech.config;
 
+import com.daxtech.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class SpringMvcSupport extends WebMvcConfigurationSupport {
 
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         //当访问路径静态页面时，不去springMVC容器中找，而是直接去webapp目录下找
         registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/**");
     }
 }
