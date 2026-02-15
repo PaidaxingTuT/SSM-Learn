976a8bef7f69a16c0fdf57d39e280c20f46cfc60
AmaneKanata <paidaxing_tut@outlook.com>
2026-02-15 16:58:43 +0800

    feat 条件分页查询+格式优化

--- SpringBoot/src/test/java/com/daxtech/springboot/ApplicationTests.java
+++ SpringBoot/src/test/java/com/daxtech/springboot/ApplicationTests.java
@@ -1,8 +1,10 @@
 package com.daxtech.springboot;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.daxtech.springboot.Mapper.BookMapper;
+import com.daxtech.springboot.Pojo.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,18 +15,19 @@ class ApplicationTests {
     @Autowired
     BookMapper bookMapper;
 
+    Book book = new Book();
+
+
     @Test
     void textByPages() {
 
         LambdaQueryWrapper<Book> lqw = new LambdaQueryWrapper<>();
-        lqw.lt(Book::getId, 1).or().gt(Book::getId, 3);
+        lqw.lt(Book::getId, 3)
+           .gt(Book::getId, 1);
         IPage page = new Page(1, 2);
-        bookMapper.selectPage(page, null);
-        System.out.println("当前页：" + page.getCurrent());
-        System.out.println("每页显示条数：" + page.getSize());
-        System.out.println("总页数：" + page.getPages());
-        System.out.println("总记录数：" + page.getTotal());
-        System.out.println("数据：" + page.getRecords());
+        bookMapper.selectPage(page, lqw);
+        System.out.println(page.getRecords());
+
     }
 
 }
